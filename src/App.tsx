4c01044e10a569f34f0572ac880d2262b46ee488
MillanUka <ukamillan@gmail.com>
2025-06-24 18:01:05 +1200

    Initial changes

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Millan Uka</h1>
+      <p>I'm a Fullstack Software Engineer based in Auckland, New Zealand.</p>
+      <p>I work mainly with Go, Java, TypeScript, React and AWS.</p>
+      <h4>You can find me on these other sites: </h4>
+      <div>
+        <a href="https://github.com/MillanUka">Github</a>
+      </div>
+
+      <div>
+        <a href="https://www.linkedin.com/in/millanuka">Linkedin</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
